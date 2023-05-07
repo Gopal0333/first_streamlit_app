13f302c53c3bea4e407dfd00273f73a7605fb5c2
Gopal0333 <130978823+Gopal0333@users.noreply.github.com>
2023-05-07 17:53:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ if streamlit.button("Add a fruit to the List"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   backfrom_function = insert_row(add_myfruit)
   streamlit.text(backfrom_function)
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit List: Add your Favourites")
 if streamlit.button("Get fruitlist"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
